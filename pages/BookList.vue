a997ea5879d4f62cd42f717a51519c0b515740b9
MarkAlexI <markovaleksandr108@gmail.com>
2024-02-04 11:01:02 +0200

    Added first client and server routes.

---
+++ pages/BookList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{ book.title }} - {{ book.author }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps([
+  'title',
+  'books'
+]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="flex flex-col items-center justify-center" style="height: 100vh">
-    <h1 class="text-center">Welcome to the first page!</h1>
-    <NuxtLink to="/">There!</NuxtLink>
+    <h1 class="text-center">Головна сторінка</h1>
+
+    <section>
+      <h2>Фічери</h2>
+      <BookList :title="'Фічери'" :books="featuredBooks" />
+    </section>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import BookList from './BookList.vue';
+
+const featuredBooks = ref([]);
+
+onMounted(() => {
+  featuredBooks.value = [
+    {
+      "id": "0",
+      "title": "Lord of the rings",
+      "author": "R.R. Tolkien"
+    },
+    {
+      "id": "0",
+      "title": "Кобзар",
+      "author": "Т.Г. Шевченко"
+    }
+  ];
+});
+</script>
